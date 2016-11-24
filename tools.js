0c354f6f92cb8c7f443e4b07a86c12a0f6391f0b
AndyRoyal <419656532@qq.com>
2016-11-24 09:59:03 +0800

    ie6 hack fix fn

--- tools.js
+++ tools.js
@@ -8,6 +8,32 @@ function lastStr(str){
         : str == "" ? ""
         : str.charAt(str.length-1);
 }
+
+function isStringToken(token){
+    var hstr = headStr(token);
+    var lstr = lastStr(token);
+
+   // ( (hstr === '"') && (lstr === '"')  ) ?  (return true) :  false;
+   // ( (hstr === "'") && (lstr === "'")  ) ?  (return true) :  false;
+
+
+    if(hstr === '"'){
+        if(lstr === '"'){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(hstr === "'"){
+        if(lstr === "'"){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+};
+isStringToken("''");
 function tailStr(str){
     return str == null ? null
         : str == "" ? ""
@@ -27,4 +53,22 @@ function takeStr(len,str){
 }
 function dropStr(len,str){
     return str.slice(len);
+}
+
+
+
+/**
+ * browser hack
+ * @private
+ */
+function _hack() {
+    // 修复ie6下javascript协议的a标签会造成图片加载停止和ajax取消
+    if (isIE6) {
+        $(document).on('click', 'a', function (e) {
+            var href = this.href + '';
+            if (href.indexOf('javascript:') == 0) {
+                e.preventDefault();
+            }
+        });
+    }
 }
