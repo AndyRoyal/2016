ffdac4e304c06c863845814ac9e5f895dd5e119e
AndyRoyal <419656532@qq.com>
2016-11-25 11:02:11 +0800

    trim

--- tools.js
+++ tools.js
@@ -1,3 +1,35 @@
+//arr 偏移量   与每个元素的处理
+function trim(str){ //删除左右两端的空格
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+function ltrim(str){ //删除左边的空格
+    return str.replace(/(^\s*)/g,"");
+}
+function rtrim(str){ //删除右边的空格
+    return str.replace(/(\s*$)/g,"");
+}
+
+trim(' a b  ');
+function trimArr(arr){
+    for(var i=0;i<arr.length;i++){
+        arr[i] = arr[i].trim();
+    }
+    return arr;
+};
+
+trimArr(["a a "," b ","   c       "]);//["a a", "b", "c"]
+
+
+
+
+
+
+
+
+
+
+
+//str 
 function headStr(str){
     return str == null ? null
         : str == "" ? ""
